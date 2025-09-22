31c6b0de8f58c9105f9de56ec49b6fd0a90e2b44
todanh <todanh2706@gmail.com>
2025-09-22 13:33:45 +0700

    Final

--- src/components/product/video.jsx
+++ src/components/product/video.jsx
@@ -21,14 +21,14 @@ export default function Video() {
                 <div className="flex flex-col justify-center items-center flex-1 p-6 bg-yellow-100">
                     <h2 className="text-xl font-semibold mb-4">How Radwich is Made</h2>
                     <a
-                        // href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-                        src="https://www.youtube.com/watch?v=95tANHqwzAc"
+                        href="https://www.youtube.com/watch?v=95tANHqwzAc"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="px-6 py-3 rounded-full bg-orange-300 text-gray-800 font-semibold hover:bg-orange-400 transition-colors duration-200"
                     >
                         Watch Full Video
                     </a>
+
                 </div>
             </div>
         </section>
